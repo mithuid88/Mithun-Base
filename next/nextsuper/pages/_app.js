92b3c1c6ad5320cbeaf90835132b8a3ea74dc09e
mithun_ui <mithun.kumar@indegene.com>
2023-12-05 16:04:42 +0530

    nextjs : adding header component / bootstrap 5

--- next/nextsuper/pages/_app.js
+++ next/nextsuper/pages/_app.js
@@ -1,5 +1,18 @@
+
+import "bootstrap/dist/css/bootstrap.css"
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof document !== 'undefined') {
+      require("bootstrap/dist/js/bootstrap");
+    }
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
 }
